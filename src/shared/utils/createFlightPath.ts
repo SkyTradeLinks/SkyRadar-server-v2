f60734d9b29fc66317db1bcb8d7959ba20cea224
chinkauchenna2021 <uchennachinka4@gmail.com>
2024-04-02 05:58:28 -0700

    clear comments

--- src/shared/utils/createFlightPath.ts
+++ src/shared/utils/createFlightPath.ts
@@ -12,26 +12,17 @@ export function extractFlightPath(json: JsonObject[]): [][] {
 }
 
 export function fetchUniqueData(jsonData: JsonObject[]): JsonObject[] {
-  // Create an empty object to keep track of unique macAddresses
   const uniqueMacAddresses = {};
 
-  // Use the filter method to iterate over the array
   const filteredData = jsonData.filter((item) => {
-    // Get the macAddress property of the current item
     const macAddress = item.remoteData.macAddress;
 
-    // Check if the macAddress already exists in the uniqueMacAddresses object
     if (uniqueMacAddresses[macAddress]) {
-      // If the macAddress exists, return false to filter out the duplicate item
       return false;
     } else {
-      // If the macAddress doesn't exist, add it to the uniqueMacAddresses object
       uniqueMacAddresses[macAddress] = true;
-      // Return true to keep the item in the filtered array
       return true;
     }
   });
-
-  // Return the filtered array
   return filteredData;
 }
