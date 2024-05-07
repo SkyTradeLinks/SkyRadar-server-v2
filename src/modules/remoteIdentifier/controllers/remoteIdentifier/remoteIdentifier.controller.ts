21f4e132b152e43d01d2b9872c7bd88898d78e61
chuksy92 <chuksud92@gmail.com>
2024-05-07 15:36:55 +0100

    configure-deviiClient

--- src/modules/remoteIdentifier/controllers/remoteIdentifier/remoteIdentifier.controller.ts
+++ src/modules/remoteIdentifier/controllers/remoteIdentifier/remoteIdentifier.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Get,
   Post,
   // UseInterceptors,
   UsePipes,
@@ -9,6 +10,7 @@ import {
 import { RemoteIdentifierService } from '../../services/remoteIdentifier/remoteIdentifier.service';
 import { ApiTags, ApiOperation, ApiCreatedResponse } from '@nestjs/swagger';
 import { RemoteIdentifierDto } from '../../dtos/remoteIdentifier.dto';
+import { DeviiClient } from 'src/graphQL/deviiClient';
 // import { BackendInterceptor } from 'src/common/interceptors/backend.interceptor';
 
 @Controller('remoteIdentifier')
@@ -26,5 +28,22 @@ export class RemoteIdentifierController {
     return this.remoteIdentifierService.createRemoteIdentifierService(
       remotedata,
     );
+  }
+
+  @Get('graphql/getDroneData')
+  @ApiOperation({ summary: 'Fetch drones data' })
+  async getDronesData(@Body() data: any) {
+    const deviiClient = new DeviiClient('https://api.devii.io/auth');
+    await deviiClient.login({
+      login: 'chukkyc582@gmail.com',
+      password: 'rainud77@',
+      tenantid: '10151',
+    });
+    try {
+      const res = await deviiClient.getDroneData(data.query);
+      return res;
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
