76508b6cdf6546bdc4f360fd260d60f52908c6f6
chuksy92 <chuksud92@gmail.com>
2024-06-16 22:13:55 +0100

    disable composeDB

--- src/compose-db_client/compose-db_client.service.ts
+++ src/compose-db_client/compose-db_client.service.ts
@@ -9,39 +9,68 @@ export class ComposeDbClientService {
     const now = new Date();
     const compose = await new Ceramic().composeDB();
 
-    try {
-      const createDrone = await compose.executeQuery(`
-      mutation CreateRemoteData{
-        createRemoteData(input: {
-          content: {
-            macAddress: "${remoteData.connection.macAddress}",
-            lastSeen: "${remoteData.connection.lastSeen}",
-            firstSeen: "${remoteData.connection.firstSeen}",
-            transportType: "${remoteData.connection.transportType}",
-            status: ${remoteData.location.status},
-            created: "${now.toISOString()}"
-            longitude: ${remoteData.location.longitude}
-            latitude: ${remoteData.location.latitude}
-
-          }
-        }){
-          document{
-            macAddress
-            lastSeen
-            firstSeen
-            transportType
-            status
-            created
-            longitude
-            latitude
+    if (process.env.IS_CERAMIC_ACTIVE) {
+      try {
+        const createDrone = await compose.executeQuery(`
+        mutation CreateRemoteData{
+          createRemoteData(input: {
+            content: {
+              macAddress: "${remoteData.connection.macAddress}",
+              lastSeen: "${remoteData.connection.lastSeen}",
+              firstSeen: "${remoteData.connection.firstSeen}",
+              transportType: "${remoteData.connection.transportType}",
+              operatorId: {
+                operatorId: "${remoteData.operatorId}"
+              },
+              identification1: {
+                uasId: "${remoteData.identification1.uasId}",
+                idType: "${remoteData.identification1.idType}",
+                uaType: "${remoteData.identification1.uaType}"
+              },
+              identification2: {
+                uasId: "${remoteData.identification2.uasId}",
+                idType: "${remoteData.identification2.idType}",
+                uaType: "${remoteData.identification2.uaType}"
+              },
+              status: ${remoteData.location.status},
+              created: "${now.toISOString()}"
+              longitude: ${remoteData.location.longitude}
+              latitude: ${remoteData.location.latitude}
+            }
+          }){
+            document{
+              macAddress
+              lastSeen
+              firstSeen
+              transportType
+              operatorId {
+                operatorId
+              }
+              identification1 {
+                uasId
+                idType
+                uaType
+              }
+              identification2 {
+                uasId
+                idType
+                uaType
+              }
+              status
+              created
+              longitude
+              latitude
+            }
           }
         }
-      }
-    `);
+      `);
 
-      return createDrone;
-    } catch (error) {
-      console.log(error);
+        return createDrone;
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      return 'Creamic network is inactive. Pls try again later!!';
     }
   }
 
@@ -51,56 +80,73 @@ export class ComposeDbClientService {
     lon2: string;
     lat2: string;
   }) {
-    try {
-      const compose = await new Ceramic().composeDB();
+    if (process.env.IS_CERAMIC_ACTIVE) {
+      try {
+        const compose = await new Ceramic().composeDB();
 
-      const droneData = await compose.executeQuery(
-        `
-      query FilterByLongitude($input: RemoteDataFiltersInput!) {
-        remoteDataIndex(filters: $input, first: 50) {
-          edges {
-            node {
-              longitude
-              latitude
-              firstSeen
-              lastSeen
-              status
+        const droneData = await compose.executeQuery(
+          `
+        query FilterByLongitude($input: RemoteDataFiltersInput!) {
+          remoteDataIndex(filters: $input, first: 50) {
+            edges {
+              node {
+                longitude
+                latitude
+                firstSeen
+                lastSeen
+                status
+                operatorId {
+                  operatorId
+                }
+                identification1 {
+                  uasId
+                  idType
+                  uaType
+                }
+                identification2 {
+                  uasId
+                  idType
+                  uaType
+                }
+              }
             }
           }
         }
-      }
-    `,
-        {
-          input: {
-            or: [
-              {
-                where: {
-                  longitude: {
-                    equalTo: parseFloat(params.lon1),
-                  },
-                  latitude: {
-                    equalTo: parseFloat(params.lat1),
-                  },
-                },
-              },
-              {
-                where: {
-                  longitude: {
-                    equalTo: parseFloat(params.lon2),
-                  },
-                  latitude: {
-                    equalTo: parseFloat(params.lat2),
+      `,
+          {
+            input: {
+              or: [
+                {
+                  where: {
+                    longitude: {
+                      equalTo: parseFloat(params.lon1),
+                    },
+                    latitude: {
+                      equalTo: parseFloat(params.lat1),
+                    },
                   },
                 },
-              },
-            ],
+                {
+                  where: {
+                    longitude: {
+                      equalTo: parseFloat(params.lon2),
+                    },
+                    latitude: {
+                      equalTo: parseFloat(params.lat2),
+                    },
+                  },
+                },
+              ],
+            },
           },
-        },
-      );
+        );
 
-      return droneData;
-    } catch (error) {
-      throw new Error('Failed to fetch data from composeDB, Try again!!');
+        return droneData;
+      } catch (error) {
+        throw new Error('Failed to fetch data from composeDB, Try again!!');
+      }
+    } else {
+      return 'Creamic network is inactive. Pls try again later!!';
     }
   }
 }
